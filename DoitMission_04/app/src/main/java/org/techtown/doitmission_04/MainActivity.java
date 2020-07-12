d9d39b0f7dbb77370950bcd459ae5d863abcd505
howtolivelikehuman <yoicch19@gmail.com>
2020-07-13 01:21:42 +0900

    DoitMission_04

--- DoitMission_04/app/src/main/java/org/techtown/doitmission_04/MainActivity.java
+++ DoitMission_04/app/src/main/java/org/techtown/doitmission_04/MainActivity.java
@@ -6,20 +6,52 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.DialogInterface;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.UnsupportedEncodingException;
+
 public class MainActivity extends AppCompatActivity {
 
-    EditText textView;
+    EditText editText;
+    TextView textView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = (EditText) findViewById(R.id.textView);
+        editText = (EditText) findViewById(R.id.textView);
+        textView2 = (TextView) findViewById(R.id.textView2);
+
+        editText.addTextChangedListener(new TextWatcher() {
+            String beforeText;
+
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                beforeText = s.toString();
+            }
+
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            public void afterTextChanged(Editable s) {
+                int length = showBytes();
+                if(length > 80) editText.setText(beforeText);
+            }
+        });
+    }
+    private int showBytes(){
+        try{
+            int length = editText.getText().toString().getBytes("EUC-KR").length;
+            textView2.setText(length + " / 80 바이트");
+            return length;
+        }catch(UnsupportedEncodingException e){
+            e.printStackTrace();
+        }
+        return -1;
     }
 
     public void onbuttonClose(View v){
@@ -43,12 +75,11 @@ public class MainActivity extends AppCompatActivity {
         alert.setTitle("종료 알림창");
         alert.show();
     }
-
     public void onbuttonSend(View v){
-       String s =  textView.getText().toString();
+       String s =  editText.getText().toString();
        //토스트 사용
        final Toast toast = Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG);
        toast.show();
-       textView.setText("");
+       editText.setText("");
     }
 }
